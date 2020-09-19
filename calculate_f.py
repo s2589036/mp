34608794e616104fecce7f11539c4b205fc69c1f
s2589036 <nielsje@planet.nl>
2020-09-19 17:23:32 +0200

    fixed a bug: it did not count the freq, now it does

--- calculate_f.py
+++ calculate_f.py
@@ -7,21 +7,26 @@ for filename in sorted(allfiles):
         tagdict = {"n":0,"adj":0,"vz":0,"lid":0,"vnw":0,"ww":0,"bw":0,"tsw":0}
         for line in onegramfile.readlines():
             tag = line.split(" ")[1].split("(")[0].lower()
+            freq = eval(line.split(" ")[1].split("\t")[1].lower())
 
             if tag in tagdict:
-                tagdict[tag]+=1
+                tagdict[tag]+=freq
             else:
-                tagdict[tag] = 1
+                tagdict[tag] = freq
+
 
         possum = tagdict['n'] + tagdict['adj'] + tagdict['vz'] + tagdict['lid']  + tagdict['vnw'] + tagdict['ww'] + tagdict['bw'] + tagdict['tsw']
 
-        fscore = ((tagdict['n']/possum)*100 +
+        fscore = (((tagdict['n']/possum)*100 +
                    (tagdict['adj']/possum)*100 +
                    (tagdict['vz']/possum)*100 +
                    (tagdict['lid']/possum)*100  -
                    (tagdict['vnw']/possum)*100  -
                    (tagdict['ww']/possum)*100 -
                    (tagdict['bw']/possum)*100 -
-                   (tagdict['tsw']/possum)*100)
+                   (tagdict['tsw']/possum)*100 +
+                   100)
+                  /2)
 
-        print(filename,'\t', fscore)
+        print(filename.split("_",1)[1].split(".")[0],'\t', fscore)
+        print(tagdict)
